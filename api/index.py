73a76618207d416e1fb9c65e8cded74904a7a46b
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-12 15:37:55 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -24,5 +24,5 @@ def serve_data():
         logging.error(f"Error reading Excel file: {e}")
         return jsonify({"error": str(e)}), 500
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
