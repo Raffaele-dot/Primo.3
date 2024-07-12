1fb8924beed73f2663286a874498108fd16f6f0b
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-12 15:44:30 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import pandas as pd
 import logging
 
@@ -17,7 +17,14 @@ def serve_data():
         excel_file = 'api/addresses.xlsx'
         df = pd.read_excel(excel_file)
         logging.info(f"Excel file read successfully. Data: {df.head()}")
-        data = df.to_dict(orient='records')
+        
+        # Pagination
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 10, type=int)
+        start = (page - 1) * per_page
+        end = start + per_page
+        
+        data = df.iloc[start:end].to_dict(orient='records')
         logging.info(f"Data to be sent: {data}")
         return jsonify(data)
     except Exception as e:
