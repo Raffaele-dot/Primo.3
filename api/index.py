b1ca75a8fa654d2fa43c609a91d0cdfb4fd32f73
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-12 15:29:25 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,11 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
+import logging
 
 app = Flask(__name__, template_folder='../templates')
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def serve_index():
@@ -9,7 +13,16 @@ def serve_index():
 
 @app.route('/data')
 def serve_data():
-    excel_file = 'api/addresses.xlsx'
-    df = pd.read_excel(excel_file)
-    data = df.to_dict(orient='records')
-    return jsonify(data)
+    try:
+        excel_file = 'api/addresses.xlsx'
+        df = pd.read_excel(excel_file)
+        logging.info(f"Excel file read successfully. Data: {df.head()}")
+        data = df.to_dict(orient='records')
+        logging.info(f"Data to be sent: {data}")
+        return jsonify(data)
+    except Exception as e:
+        logging.error(f"Error reading Excel file: {e}")
+        return jsonify({"error": str(e)}), 500
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
