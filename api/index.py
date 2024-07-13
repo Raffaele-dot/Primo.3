1a36ebcf603b48587d0523610b1e50d993802a8e
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-13 19:55:03 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,6 +18,8 @@ def serve_data_within_bounds():
         northEastLng = request.args.get('northEastLng', type=float)
         southWestLat = request.args.get('southWestLat', type=float)
         southWestLng = request.args.get('southWestLng', type=float)
+        
+        logging.info(f"Received bounds: NE({northEastLat}, {northEastLng}), SW({southWestLat}, {southWestLng})")
 
         excel_file = 'api/addresses.xlsx'
         df = pd.read_excel(excel_file)
@@ -33,7 +35,7 @@ def serve_data_within_bounds():
         logging.info(f"Filtered data to be sent: {data}")
         return jsonify(data)
     except Exception as e:
-        logging.error(f"Error reading Excel file: {e}")
+        logging.error(f"Error processing request: {e}")
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
