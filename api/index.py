28982c49f7455de1efa85afe82087765a14fe33c
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-13 20:01:09 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -18,8 +18,10 @@ def serve_data_within_bounds():
         northEastLng = request.args.get('northEastLng', type=float)
         southWestLat = request.args.get('southWestLat', type=float)
         southWestLng = request.args.get('southWestLng', type=float)
-        
-        logging.info(f"Received bounds: NE({northEastLat}, {northEastLng}), SW({southWestLat}, {southWestLng})")
+        page = request.args.get('page', 1, type=int)
+        per_page = request.args.get('per_page', 100, type=int)
+
+        logging.info(f"Received bounds: NE({northEastLat}, {northEastLng}), SW({southWestLat}, {southWestLng}), Page: {page}, Per Page: {per_page}")
 
         excel_file = 'api/addresses.xlsx'
         df = pd.read_excel(excel_file)
@@ -29,10 +31,15 @@ def serve_data_within_bounds():
         df = df.fillna("")
 
         # Filter data within bounds
-        df = df[(df['Latitude'] >= southWestLat) & (df['Latitude'] <= northEastLat) & (df['Longitude'] >= southWestLng) & (df['Longitude'] <= northEastLng)]
+        filtered_df = df[(df['Latitude'] >= southWestLat) & (df['Latitude'] <= northEastLat) & (df['Longitude'] >= southWestLng) & (df['Longitude'] <= northEastLng)]
+        
+        # Paginate the filtered data
+        start = (page - 1) * per_page
+        end = start + per_page
+        data = filtered_df.iloc[start:end].to_dict(orient='records')
 
-        data = df.to_dict(orient='records')
-        logging.info(f"Filtered data to be sent: {data}")
+        logging.info(f"Filtered data to be sent: {data[:5]}... (and more)")
+
         return jsonify(data)
     except Exception as e:
         logging.error(f"Error processing request: {e}")
