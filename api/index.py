87d649643b589a22968d876de386b49a38d8edfe
Raffaele-dot <134276058+Raffaele-dot@users.noreply.github.com>
2024-07-15 19:42:35 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,25 @@
 from flask import Flask, jsonify, render_template, request
 import pandas as pd
 import logging
-import json  # Added import for JSON
+import os
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 # Configure logging
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def serve_index():
-    return render_template('index.html')
+    try:
+        # Log the current working directory and file structure
+        logging.debug(f"Current working directory: {os.getcwd()}")
+        logging.debug(f"Files in template folder: {os.listdir('../templates')}")
+        logging.debug(f"Files in static folder: {os.listdir('../static')}")
+        
+        return render_template('index.html')
+    except Exception as e:
+        logging.error(f"Error rendering template: {e}")
+        return jsonify({"error": str(e)}), 500
 
 @app.route('/data-within-bounds')
 def serve_data_within_bounds():
